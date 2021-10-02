5cbc3fe61dedc7d122770e2494908ec5352660d0
Victoria Molina <victoria.molina89@gmail.com>
2021-10-02 13:32:01 +0200

    primera llamada ajax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
   console.log("Hola lucius");
+  var json;
 
-  $('#imgDivComun').slick({
-    dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
+  $.getJSON('JSON/main.json', function(objJson, status) {
+      json = objJson;
+      fillHtml();
   });
+
+  function fillHtml() {
+    console.log(json);
+
+    $.each(json, function(index, field) {
+      if (field.autor == "Lu" ) {
+        console.log(field.autor + " / " + field.name);
+        $("#jLu").html("<img src='" + field.url + "'>");
+      } else if (field.autor == "Vicky" ) {
+        console.log(field.autor + " / " + field.name);
+        $("#jVicky").html("<img src='" + field.url + "'>");
+      } else {
+        console.log(field.autor + " / " + field.name);
+        $("#jComun").html("<img src='" + field.url + "'>");
+  }
+    });
+  }
+
 });
