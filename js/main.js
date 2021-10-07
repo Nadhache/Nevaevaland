7e6d82f3bb9ccbee1551c62ef465ce015567250e
Victoria Molina <victoria.molina89@gmail.com>
2021-10-07 11:05:59 +0200

    a galeria

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  console.log("Hola lucius");
+  // Inicializacion variable para guardar el json
   var json;
 
+  // Llamada a JSON
   $.getJSON('JSON/main.json', function(objJson, status) {
     json = objJson;
     fillHtml();
   });
 
+  // Cargamos las imagenes del home
   function fillHtml() {
-    console.log(json);
-
     $.each(json, function(index, field) {
       if (field.autor == "Lu") {
         console.log(field.autor + " / " + field.name);
@@ -24,7 +24,39 @@ $(document).ready(function() {
     });
   }
 
+  //Escuchadores de Galleria
+  $("#gVicky").click(function() {
+    console.log("Hola soy galeria de vicky");
+    printPictures("Vicky");
+  });
 
+  $("#gLu").click(function() {
+    console.log("Hola galeria de lu");
+    printPictures("Lu");
+  });
+
+  $("#gComun").click(function() {
+    console.log("Hola galeria comun");
+    printPictures("Comun");
+  });
+
+  //Carga de galeria
+  function printPictures(autors) {
+    // console.log(autors);
+    var htmlStr = "<div>";
+    $.each(json, function(index, el) {
+      if (el.autor == autors) {
+        console.log(el.autor + " / " + el.name);
+        htmlStr = htmlStr + "<img src='" + el.url + "'>";
+      } else {
+        console.log("imagen no de autor");
+      }
+    });
+    htmlStr = htmlStr + "</div>";
+    console.log(htmlStr);
+    $("main").html(htmlStr);
+
+  }
 
 
 
